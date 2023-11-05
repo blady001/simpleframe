28aac7ed13d2e44e05f556e8c101ea37890bff58
Dawid Brzyszcz <dawid.brzyszcz@hotmail.com>
2023-11-05 16:28:15 +0100

    Border calculations are correct now, still, initial scaling is incorrect

--- simpleframe/CanvasView.swift
+++ simpleframe/CanvasView.swift
@@ -13,9 +13,10 @@ struct CanvasView: View {
     
     @Binding var image: UIImage?
     
-    @State private var sliderValue: CGFloat = 0
+    @State private var borderPercentage: CGFloat = 0
     
     var MAX_BORDER_SIZE_IN_PERCENTAGE: CGFloat = 10
+    var SLIDER_STEP: CGFloat = 1
     
     var body: some View {
         VStack {
@@ -25,16 +26,18 @@ struct CanvasView: View {
                     if let image = image {
 //                        let imgMaxFrameSize = getImgMaxFrameSize(canvasSize: geometry.size, image: image)
                         let imgDisplaySize = getImgMaxSize(imageSize: image.size, containerSize: geometry.size)
-                        let imgDisplayFrameSize = imgDisplaySize
-                        //                        let imgDisplayFrameSize = getImgFrameScaled(imageMaxSize: imgMaxFrameSize)
-//                        let rectSize = getRectSize(imageSize: imageSize)
-//                        Rectangle()
-//                            .fill(.red)
-//                            .frame(width: rectSize.width, height: rectSize.height)
-//                            .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+                        let imgDisplayFrameSize = downscaleAlongLongerDimension(source: imgDisplaySize, scale: getDisplayImgScale())
+//                        let borderRectSize = downscaleAlongLongerDimension(source: imgDisplaySize, scale: getRectScale())
+                        let borderRectSize = getBorderRectSize(imageSize: imgDisplayFrameSize, borderSizePercentage: borderPercentage)
+//                        let _ = print("Frame: \(geometry.size)")
+//                        let _ = print("ImgMax: \(imgDisplaySize)")
+//                        let _ = print("ImgScale: \(imgDisplayFrameSize)")
+                        Rectangle()
+                            .fill(.red)
+                            .frame(width: borderRectSize.width, height: borderRectSize.height)
+                            .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
                         Image(uiImage: image)
                             .resizable()
-//                            .scaledToFit()
                             .frame(width: imgDisplayFrameSize.width, height: imgDisplayFrameSize.height)
                             .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
     //                            .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
@@ -45,7 +48,7 @@ struct CanvasView: View {
             }
             .background(Color.orange)
             VStack {
-                Slider(value: $sliderValue, in: 0...MAX_BORDER_SIZE_IN_PERCENTAGE, step: 1)
+                Slider(value: $borderPercentage, in: 0...MAX_BORDER_SIZE_IN_PERCENTAGE, step: SLIDER_STEP)
             }
             .background(Color.purple)
         }
@@ -67,40 +70,17 @@ struct CanvasView: View {
     }
     
     private func dismissView() {
-//        image = nil
+        image = nil
         presentationMode.wrappedValue.dismiss()
     }
     
-//    private func getImgMaxFrameSize(canvasSize: CGSize, image: UIImage) -> CGSize {
-//        
-//        let d: CGFloat = 1 + 2.0 * (MAX_BORDER_SIZE_IN_PERCENTAGE / 100.0)
-//        
-//        // real image dimensions
-//        let rx = image.size.width
-//        let ry = image.size.height
-//        
-//        // canvas screen dimensions
-//        let cx = canvasSize.width
-//        let cy = canvasSize.height
-//        
-//        let cyx = cy / cx
-//        let ryx = ry / rx
-//        
-//        let ix, iy: CGFloat
-//        
-//        // TODO: currently "d" is calculated not per greatest side, but per alignment either to top or to side
-//        // This should be changed, so that the image border is restricted by its longer side
-//        if (cyx < ryx) {
-//            // image is horizontal
-//            ix = (cy * rx) / (d * ry)
-//            iy = cy / d
-//        } else {
-//            // image is vertical
-//            ix = cx / d
-//            iy = (cx * ry) / (d * rx)
-//        }
-//        return CGSize(width: ix, height: iy)
+//    private func getDisplayImgScale() -> CGFloat {
+//        return (100.0 - 2 * MAX_BORDER_SIZE_IN_PERCENTAGE) / 100.0
 //    }
+    
+    private func getDisplayImgScale() -> CGFloat {
+        return 100.0 / (100.0 + 2 * MAX_BORDER_SIZE_IN_PERCENTAGE)
+    }
     
     private func getImgMaxSize(imageSize: CGSize, containerSize: CGSize) -> CGSize {
         // real image dimensions
@@ -129,71 +109,47 @@ struct CanvasView: View {
         return CGSize(width: ix, height: iy)
     }
     
-//    private func getImgMaxFrameSize(canvasSize: CGSize, image: UIImage) -> CGSize {
-//        // real image dimensions
-//        let rx = image.size.width
-//        let ry = image.size.height
-//        
-//        // canvas screen dimensions
-//        let cx = canvasSize.width
-//        let cy = canvasSize.height
-//        
-//        let cyx = cy / cx
-//        let ryx = ry / rx
-//        
-//        let ix, iy: CGFloat
-//        
-//        // TODO: currently "d" is calculated not per greatest side, but per alignment either to top or to side
-//        // This should be changed, so that the image border is restricted by its longer side
-//        if (cyx < ryx) {
-//            // image is horizontal
-//            ix = (cy * rx) / ry
-//            iy = cy
-//        } else {
-//            // image is vertical
-//            ix = cx
-//            iy = (cx * ry) / rx
-//        }
-//        return CGSize(width: ix, height: iy)
-//    }
-    
-    private func getImgFrameScaled(imageMaxSize: CGSize) -> CGSize {
-        let ix = imageMaxSize.width
-        let iy = imageMaxSize.height
+    private func downscaleAlongLongerDimension(source: CGSize, scale: CGFloat) -> CGSize {
+        if (scale == 1.0) {
+            return CGSize(width: source.width, height: source.height)
+        }
+        
+        let ix = source.width
+        let iy = source.height
         
         let sx, sy: CGFloat
         
-        let d: CGFloat = (100 + 2 * MAX_BORDER_SIZE_IN_PERCENTAGE) / 100
-        
-        if (ix > iy) {
-            sx = ix / d
+        if (source.width > source.height) {
+            // scale along X
+            sx = scale * ix;
             sy = sx * iy / ix
         } else {
-            sy = iy / d
-            sx = ix * sy / iy
+            // scale along Y
+            sy = scale * iy
+            sx = sy * ix / iy
         }
-        
         return CGSize(width: sx, height: sy)
     }
     
-    private func getRectSize(imageSize: CGSize) -> CGSize {
-        let d: CGFloat = sliderValue / 100.0
-        
+    private func getBorderRectSize(imageSize: CGSize, borderSizePercentage: CGFloat) -> CGSize {
         let ix = imageSize.width
         let iy = imageSize.height
         
-        let b: CGFloat
+        let scaleFactor: CGFloat = (100.0 + 2.0 * borderSizePercentage) / 100.0
         
-        if (iy > ix) {
-            // image is horizontal
-            b = d * iy
+        let bx, by: CGFloat
+        
+        if (ix > iy) {
+            bx = scaleFactor * ix
+            by = iy + bx - ix
         } else {
-            // image is vertical
-            b = d * ix
+            by = scaleFactor * iy
+            bx = ix + by - iy
         }
         
-        return CGSize(width: ix + 2 * b, height: iy + 2 * b)
+        return CGSize(width: bx, height: by)
     }
+    
 }
 
 #Preview {
